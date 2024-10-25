bf12c6469aecdd3a8f38eb4c49f0d4d5b06f656b
Pablo Luque Salguero <pablols114@gmail.com>
2024-10-25 10:02:54 +0200

    style: apply clangd format

--- src/grafo-escena.h
+++ src/grafo-escena.h
@@ -1,14 +1,16 @@
 // *********************************************************************
 // **
 // ** Asignatura: INFORMÁTICA GRÁFICA
-// ** 
-// ** Gestión de grafos de escena (declaraciones).  Clase 'NodoGrafoEscena' y relacionadas.
+// **
+// ** Gestión de grafos de escena (declaraciones).  Clase 'NodoGrafoEscena' y
+// relacionadas.
 // ** Copyright (C) 2016-2024 Carlos Ureña
 // **
-// ** Declaraciones de: 
+// ** Declaraciones de:
 // **     + Clase 'NodoGrafoEscena' (derivada de 'Objeto3D')
 // **     + Clase 'EntradaNGE' (una entrada de un nodo del grafo de escena)
-// **     + Tipo enumerado 'TipoEntNGE' (tipo de entradas del nodo del grafo de escena)
+// **     + Tipo enumerado 'TipoEntNGE' (tipo de entradas del nodo del grafo de
+// escena)
 // **
 // ** This program is free software: you can redistribute it and/or modify
 // ** it under the terms of the GNU General Public License as published by
@@ -28,98 +30,91 @@
 #ifndef GRAFO_ESCENA_HPP
 #define GRAFO_ESCENA_HPP
 
-//#include <tup_mat.h>   
+// #include <tup_mat.h>
 
-#include "objeto3d.h"
 #include "malla-ind.h" // para poder usar clase MallaInd
 #include "materiales-luces.h"
+#include "objeto3d.h"
 
-//using namespace tup_mat ;
+// using namespace tup_mat ;
 
 // *********************************************************************
 // declaración adelantada de estructura para un nodo del grafo de escena
 
-class NodoGrafoEscena ;
+class NodoGrafoEscena;
 
 // *********************************************************************
 // tipo enumerado con los tipos de entradas del nodo del grafo de escena
 
-enum class TipoEntNGE { objeto, transformacion, material, noInicializado } ;
+enum class TipoEntNGE { objeto, transformacion, material, noInicializado };
 
 // *********************************************************************
 // Entrada del nodo del Grafo de Escena
 
-struct EntradaNGE
-{
-   TipoEntNGE tipo = TipoEntNGE::noInicializado ;   // objeto, transformacion, material
-   union
-   {  Objeto3D  * objeto = nullptr ;  // ptr. a un objeto (no propietario)
-      glm::mat4 * matriz   ;  // ptr. a matriz 4x4 transf. (propietario)
-      Material  * material ; // ptr. a material (no propietario)
-   } ;
-   // constructores (uno por tipo)
-   EntradaNGE() = delete ; // prohibe constructor sin parámetros
-   EntradaNGE( Objeto3D        * pObjeto   );      // (copia solo puntero)
-   EntradaNGE( const glm::mat4 & pMatriz   ); // (crea copia en el heap)
-   EntradaNGE( Material        * pMaterial );    // (copia solo puntero)
-   ~EntradaNGE() ;
-} ;
+struct EntradaNGE {
+  TipoEntNGE tipo =
+      TipoEntNGE::noInicializado; // objeto, transformacion, material
+  union {
+    Objeto3D *objeto = nullptr; // ptr. a un objeto (no propietario)
+    glm::mat4 *matriz;          // ptr. a matriz 4x4 transf. (propietario)
+    Material *material;         // ptr. a material (no propietario)
+  };
+  // constructores (uno por tipo)
+  EntradaNGE() = delete;                // prohibe constructor sin parámetros
+  EntradaNGE(Objeto3D *pObjeto);        // (copia solo puntero)
+  EntradaNGE(const glm::mat4 &pMatriz); // (crea copia en el heap)
+  EntradaNGE(Material *pMaterial);      // (copia solo puntero)
+  ~EntradaNGE();
+};
 
 // *********************************************************************
-// Nodo del grafo de escena: es un objeto 3D parametrizado, que contiene una lista de entradas
+// Nodo del grafo de escena: es un objeto 3D parametrizado, que contiene una
+// lista de entradas
 
-class NodoGrafoEscena : public Objeto3D
-{
-   protected:
-   // COMPLETAR: práctica 3: definir variables y métodos privados del nodo
-   // .......
+class NodoGrafoEscena : public Objeto3D {
+protected:
+  // COMPLETAR: práctica 3: definir variables y métodos privados del nodo
+  // .......
 
-   public:
+public:
+  NodoGrafoEscena();
 
-   NodoGrafoEscena() ;
+  // visualiza usando OpenGL
+  virtual void visualizarGL();
 
-   // visualiza usando OpenGL
-   virtual void visualizarGL(  ) ;
+  // visualizar pura y simplemente la geometría, sin colores, normales, coord.
+  // text. etc... (se supone que el estado de OpenGL está fijado antes de esta
+  // llamada de alguna forma adecuada)
+  virtual void visualizarGeomGL();
 
-   // visualizar pura y simplemente la geometría, sin colores, normales, coord. text. etc...
-   // (se supone que el estado de OpenGL está fijado antes de esta llamada de alguna forma adecuada)
-   virtual void visualizarGeomGL(  ) ;
+  // visualizar las normales de los objetos del nodo
+  virtual void visualizarNormalesGL();
 
-   // visualizar las normales de los objetos del nodo
-   virtual void visualizarNormalesGL () ;
+  // visualizar el objeto en 'modo seleccion', es decir, sin iluminación y con
+  // los colores basados en los identificadores de los objetos
+  virtual void visualizarModoSeleccionGL();
 
-   // visualizar el objeto en 'modo seleccion', es decir, sin iluminación y con los colores 
-   // basados en los identificadores de los objetos
-   virtual void visualizarModoSeleccionGL()  ;
+  // añadir una entrada al final, hace copia de la entrada
+  // devuelve indice de la entrada dentro del vector de entradas
+  unsigned agregar(const EntradaNGE &entrada);
 
-   // añadir una entrada al final, hace copia de la entrada
-   // devuelve indice de la entrada dentro del vector de entradas
-   unsigned agregar( const EntradaNGE & entrada );
+  // construir una entrada y añadirla (al final)
+  unsigned agregar(Objeto3D *pObjeto);        // objeto (copia solo puntero)
+  unsigned agregar(const glm::mat4 &pMatriz); // matriz (copia objeto)
+  unsigned agregar(Material *pMaterial);      // material (copia solo puntero)
 
-   // construir una entrada y añadirla (al final)
-   unsigned agregar( Objeto3D *        pObjeto ); // objeto (copia solo puntero)
-   unsigned agregar( const glm::mat4 & pMatriz ); // matriz (copia objeto)
-   unsigned agregar( Material *        pMaterial ); // material (copia solo puntero)
+  // devuelve el puntero a la matriz en la i-ésima entrada
+  glm::mat4 *leerPtrMatriz(unsigned iEnt);
 
-   // devuelve el puntero a la matriz en la i-ésima entrada
-   glm::mat4 * leerPtrMatriz( unsigned iEnt );
+  // método para buscar un objeto con un identificador
+  virtual bool buscarObjeto(const int ident_busc, const glm::mat4 &mmodelado,
+                            Objeto3D **objeto, glm::vec3 &centro_wc);
 
-   // método para buscar un objeto con un identificador
-   virtual bool buscarObjeto( const int ident_busc, const glm::mat4 & mmodelado,
-                    Objeto3D ** objeto, glm::vec3 & centro_wc )  ;
-
-   // si 'centro_calculado' es 'false', recalcula el centro usando los centros
-   // de los hijos (el punto medio de la caja englobante de los centros de hijos)
-   virtual void calcularCentroOC() ;
-
-
-} ;
+  // si 'centro_calculado' es 'false', recalcula el centro usando los centros
+  // de los hijos (el punto medio de la caja englobante de los centros de hijos)
+  virtual void calcularCentroOC();
+};
 
 // *********************************************************************
-
-
-
 
 #endif // GRAFO_ESCENA_HPP
-
-
